82056ed15b0817cc0bde153d34269bb500e47b9f
SATO Yozo <Yozo.Sato@gamepot.co.jp>
2013-03-29 12:29:14 +0900

    replaced English term into Japanese for minimum necessary features.

---
+++ languages/Japanese/definitions.BBCode.php
@@ -0,0 +1,13 @@
+<?php
+// Copyright 2011 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// English Definitions for the BBCode plugin.
+
+$definitions["Fixed"] = "コード";
+$definitions["Image"] = "画像";
+$definitions["Link"] = "リンク";
+$definitions["Strike"] = "打ち消し";
+$definitions["Header"] = "ヘッダ";
+$definitions["Italic"] = "斜体";
+$definitions["Bold"] = "強調";
